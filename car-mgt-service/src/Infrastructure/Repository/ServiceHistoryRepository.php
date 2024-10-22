8ec6459501d58d57a8d1a211f66fe9ff65b6a935
Jeshta Bhoyedhur <jeshta.bhoyedhur@gmail.com>
2024-10-22 12:57:29 +0400

    [Repository] Implement repository interfaces for car and serviceHistory

---
+++ car-mgt-service/src/Infrastructure/Repository/ServiceHistoryRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Infrastructure\Repository;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use App\Domain\Car\Car;
+use App\Domain\ServiceHistory\ServiceHistory;
+use App\Domain\ServiceHistory\ServiceHistoryRepositoryInterface;
+
+class ServiceHistoryRepository implements ServiceHistoryRepositoryInterface
+{
+    private EntityManagerInterface $entityManager;
+    private EntityRepository $repository;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->repository = $entityManager->getRepository(ServiceHistory::class);
+    }
+
+    public function save(ServiceHistory $serviceHistory): void
+    {
+        $this->entityManager->persist($serviceHistory);
+        $this->entityManager->flush();
+    }
+
+    public function remove(ServiceHistory $serviceHistory): void
+    {
+        $this->entityManager->remove($serviceHistory);
+        $this->entityManager->flush();
+    }
+
+    public function findByCar(Car $car): array
+    {
+        return $this->repository->findBy(['car' => $car]);
+    }
+}
